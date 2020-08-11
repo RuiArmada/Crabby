596045b2062a242e9041f1f67d899d7b14aa2776
RuiArmada <aepb.rui.98@gmail.com>
2020-08-12 00:56:59 +0100

    Commit #8

--- FlappyGina/src/gina.rs
+++ FlappyGina/src/gina.rs
@@ -115,4 +115,72 @@ impl PlayerEntity {
            player_sprites,
        } 
    }
+
+   pub fn overlaps(&self, other : &PipeEntity) -> bool {
+       let player_rect = sef.get_bounds();
+       let other_rect = other.get_rect();
+       other_rect.overlaps(&player_rect)
+   }
+
+   pub fn get_bounds(&self) -> graphics::Rect{
+       let mut rect = self.sprite.get_bound_box();
+       rect.move_to(self.position.clone());
+       rect
+   }
+
+   fn prevent_going_off(&mut self) -> (){
+       self.position.y = if self.position.y < SCREEN_TOP {
+           SCREEN_TOP
+       }else{
+        self.position.y
+       }
+   }
+
+   fn auto_jump(&mut self) -> () {
+       let physics = &mut self.physics;
+       if self.position.y > 600.0 / 8.0 {
+           PlayerEntity::jump(physics)
+       }
+   }
+
+   pub fn draw(&mut self, batch: &mut SpriteBatch) -> GameResult {
+       self.draw_player(batch);
+       Ok(())
+   }
+
+   fn draw_player(&mut self, batch: &mut SpriteBatch) {
+       let aux = &mut self.player_sprites;
+       let aux1 = &self.physics;
+       //need velocity to map to these rotations between -0.2 and 0.2
+       let angle = rescale_range(aux1.velocity.y, -7.0, 7.0, -0.6, 0.6);
+       let aux2 = if aux1.velocity,y >= 0.0 {
+           &mut aux[1]
+       }else{
+           &mut aux[0]
+       };
+       batch.add(
+           aux2.add_draw_param(self.position.clone())
+               .offset(Point2::new(0.5,0.5))
+               .rotation(angle),
+       );
+   }
+
+   fn jump(physics: &mut Physics){
+       physics.acceleration = Vector2::new(0.0, -GRAVITY);
+       physics.velocity = Vector2::new(0.0, -JUMP_IMPULSE);
+   }
 }
+
+
+
+//Returns an f32 scaled [oldMin, oldMax] into the range [newMin, newMax]
+fn rescale_range(value: f32, old_min: f32, old_max: f32, new_min: f32, new_max: f32) -> f32 {
+    use ggez::nalgebra::clamp;
+    let old_range = old_max - old_min;
+    let new_range = new_max - new_min;
+    (((clamp(value,old_min,old_max) - old_min) * new_range) / old_range) + new_min
+}
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------*/
