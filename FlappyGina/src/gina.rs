2f7f551f77182aeae9878d2c1aafca14f7861381
RuiArmada <aepb.rui.98@gmail.com>
2020-08-11 23:51:34 +0100

    Commit #7

--- FlappyGina/src/gina.rs
+++ FlappyGina/src/gina.rs
@@ -54,3 +54,65 @@ pub fn create_player(sprites: &atlas::Atlas) -> Box<PlayerEntity> {
 
     Box::new(player)
 }
+
+
+
+impl PlayerEntity {
+   pub fn update(
+       &mut self,
+       ctx: &mut Context,
+       state: &PlayState,
+   ) -> PlayState {
+       let physics = &mut self.physics;
+       physics.acceleration = if physics.gravity {
+           Vector2::new(0.0,GRAVITY)
+       }else{
+           Vector2::new(0.0,0.0)
+       };
+       let mut state = state.clone();
+       if state.is_not_dead(){
+           use ggez::event::KeyCode;
+           use ggez::input::keyboard;
+           if !keyboard::pressed_keys(ctx).contains(&KeyCode::Space) && !self.can_jump{
+               self.can_jump = true;
+           }
+           if keyboard::is_key_pressed(ctx, KeyCode::Space) && self.can_jump{
+               let physics = &mut self.physics;
+               PlayerEntity::jump(physics);
+               //exit start screen state
+               if state == PlayState::StartScreen{
+                   state = PlayState::Play;
+               }
+           }
+       }
+       //Self jumping script on Start Screen
+       if state == PlayState::StartScreen{
+           self.auto_jump()
+       }
+       self.change_player_position(ctx);
+
+       //Gina should not fly above the top of screen easily. She might get burned by the Sun :(
+       self.prevent_going_off();
+       state
+   }
+
+   fn change_player_position(&mut self, ctx:&mut Context){
+       let delta = ggez::timer::delta(ctx).as_nanos() as f32;
+       let physics = &mut self.physics;
+       physics.acceleration.scale(1.0/delta);
+       physics.velocity += physics.acceleration;
+       physics.velocity.scale(1.0/delta);
+       //moves all thing on the board
+       self.position += physics.velocity;
+   }
+
+   pub fn new(sprite: Sprite, position: (f32,f32), player_sprites: Vec<Sprite>) -> Self {
+       Self{
+           sprite,
+           position: Point2::new(position.0,position.1),
+           physics: Physics::new(true),
+           can_jump: true,
+           player_sprites,
+       } 
+   }
+}
